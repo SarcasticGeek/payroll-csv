6dd8c77f6ff468d04e2f962e6183e8d8dfe0a0a6
SarcasticGeek <mohamedessamfathalla@gmail.com>
2019-01-12 12:31:53 +0200

    finish csv prints

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const moment = require('moment');
 var program = require('commander');
-var csv_export = require('csv-export');
 var fs = require('fs');
+const Json2csvParser = require('json2csv').Parser;
+const fields = ['month', 'salary'];
+const opts = { fields };
 
 let newdate = new moment();
 
@@ -14,21 +16,58 @@ program
   .version('0.1.0')
   .option('-b, --base', 'Base Salary')
   .option('-o, --bonus', 'Bonus')
+  .option('-a, --all', 'All Saalaries')
   .parse(process.argv);
 
 
 if(program.base){
-    console.log(payroll.getBaseSalaryForNext12Months());
     let baseSalaryMonths = payroll.getBaseSalaryForNext12Months();
-    csv_export.export(baseSalaryMonths,function(buffer){
-        fs.writeFileSync('./tmp/export.zip',buffer);
-      });
+    let OutPutForCsv = [];
+    for (const key in baseSalaryMonths) {
+        if (baseSalaryMonths.hasOwnProperty(key)) {
+            let obj = {
+                "month": key,
+                "salary": "Base Salary in " + baseSalaryMonths[key]
+            }
+            OutPutForCsv.push(obj);            
+        }
+    }
+    const parser = new Json2csvParser(opts);
+    const csv = parser.parse(OutPutForCsv);
+    fs.writeFileSync('./tmp/baseSalaries.csv',csv);
 }
 
 if(program.bonus){
-    console.log(payroll.getBonusSalaryForNext12Months());
     let bonusMonths = payroll.getBonusSalaryForNext12Months();
-    csv_export.export(bonusMonths,function(buffer){
-        fs.writeFileSync('./tmp/export.zip',buffer);
-      });
+    let OutPutForCsv = [];
+    for (const key in bonusMonths) {
+        if (bonusMonths.hasOwnProperty(key)) {
+            let obj = {
+                "month": key,
+                "salary": "Bonus Salary in " + bonusMonths[key]
+            }
+            OutPutForCsv.push(obj);            
+        }
+    }
+    const parser = new Json2csvParser(opts);
+    const csv = parser.parse(OutPutForCsv);
+    fs.writeFileSync('./tmp/bonusSalaries.csv',csv);
+}
+if(program.all){
+    let baseSalaryMonths = payroll.getBaseSalaryForNext12Months();
+    let bonusMonths = payroll.getBonusSalaryForNext12Months();
+    let OutPutForCsv = [];
+    for (const key in baseSalaryMonths) {
+        if (baseSalaryMonths.hasOwnProperty(key) && bonusMonths.hasOwnProperty(key)) {
+            let obj = {
+                "month": key,
+                "salary": "Base Salary in " + baseSalaryMonths[key] +" & Bonus Salary in "+ bonusMonths[key]
+            }
+            OutPutForCsv.push(obj);
+        }
+    }
+    const parser = new Json2csvParser(opts);
+    const csv = parser.parse(OutPutForCsv);
+    fs.writeFileSync('./tmp/all.csv',csv);
+
 }
